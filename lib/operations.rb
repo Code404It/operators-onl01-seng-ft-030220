c7ea608b81ce901816af6fac22fac44ff2f93ec6
Jessica Smith <jessikahd.smith@gmail.com>
2020-03-05 02:38:53 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,10 +1,17 @@
 def unsafe?(speed)
-
+  if speed is > 60
+    retun true
+    elsif speed < 40
+    return true
+  else
+    return false
+end
 end
 
 
 
 def not_safe?(speed)
+  speed < 40 || speed > 60 ? true : false
 	
 end
 	
